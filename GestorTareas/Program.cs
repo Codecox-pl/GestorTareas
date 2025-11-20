064d03f3411894b85a5ef75a850ae7eb5ea9b896
Patrick <patricklmo32@gmail.com>
2025-11-19 20:17:24 -0500

    Modificacion del Program

--- GestorTareas/Program.cs
+++ GestorTareas/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using GestorTareas;
+
+//Creando lista vacia de Tarea
+List<Tarea> tareas = new List<Tarea>();
+
+//Tarea 1 //
+Console.WriteLine("Ingrese el nombre de la nueva tarea: ");
+string tarea1 = Console.ReadLine();
+
+//Creando objetos
+Tarea T1 = new Tarea(tarea1);
+
+//Tarea 2 //
+Console.WriteLine("Ingrese el nombre de la nueva tarea: ");
+string tarea2 = Console.ReadLine();
+
+//Creando objetos
+Tarea T2 = new Tarea(tarea2);
+
+//Tarea 2 //
+Console.WriteLine("Ingrese el nombre de la nueva tarea: ");
+string tarea3 = Console.ReadLine();
+
+//Creando objetos
+Tarea T3 = new Tarea(tarea3);
+
+//Añadiendo a la lista
+tareas.Add(T1);
+tareas.Add(T2);
+tareas.Add(T3);
+
+//Imprimiendo la cantidad de elementos
+Console.WriteLine("\n=== TUS TAREAS ===");
+foreach (var tarea in tareas)
+{
+  Console.WriteLine("Tarea:" + tarea.Titulo);  
+}
+
---
+++ GestorTareas/Tarea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorTareas
+{
+    public class Tarea
+    {
+        // 1. Propiedad para el ID (número entero)
+        public int ID { get; set; }
+
+        // 2. Propiedad para el Título (texto)
+        public string Titulo { get; set; }
+
+        // 3. Propiedad para el estado "Completada" (booleano)
+        public bool Completada { get; set; }
+
+
+        // Constructor //
+
+        public Tarea(string tituloInicial)
+        {
+            Titulo = tituloInicial;
+            Completada = false;
+        }
+    }
+}
